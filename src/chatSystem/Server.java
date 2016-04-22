b96465429b0aa3389286e020d50c8845a411710b
Swashy <Swashy@DESKTOP-8UDOU42>
2016-04-22 14:08:00 +0200

    #Lagt till en thread som hanterar en chatarea med information #Lagt till en multithreaded handler, som hanterar alla nya clienter #lagt en bra bas till chat servern och hur den kommer fungera

---
+++ src/chatSystem/Server.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatSystem;
+
+import controllers.FXMLDocumentController;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Swashy
+ */
+public class Server {
+
+    private static final int PORT = 9006;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+
+    public void CreateServer(int port) {
+
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Creating Server...");
+
+        } catch (IOException e) {
+            System.err.println("Error in creation of the server socket");
+            System.exit(0);
+        }
+    }
+
+    public void StartServer(int port) {
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+
+                    try {
+
+                        clientSocket = serverSocket.accept();
+                        System.out.println("Client Connected");
+                        chatHandler chath = new chatHandler(clientSocket);
+                        chath.start();
+
+                    } catch (IOException e) {
+                        System.out.println("Client failed to connect!");
+                    }
+                }
+            }
+        });
+        t1.start();
+
+    }
+
+    private static class chatHandler extends Thread {
+
+        private Socket clientSocket;
+
+        private chatHandler(Socket clientSocket) {
+            super("chatHandler");
+            this.clientSocket = clientSocket;
+
+        }
+    }
+
+    public void run() {
+
+        //Kör chatten här
+    }
+}
--- src/controllers/FXMLDocumentController.java
+++ src/controllers/FXMLDocumentController.java
@@ -5,32 +5,42 @@
  */
 package controllers;
 
+import chatSystem.Server;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 /**
  *
  * @author Swashy
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+    @FXML
+    private TextArea allChat;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-               
-        
-    }    
-    
+
+        chatSystem.Server server = new Server();
+        server.CreateServer(9006);
+        allChat.appendText("Creating server...\n");
+        server.StartServer(9006);
+        allChat.appendText("Starting server...\n");
+        allChat.appendText("Waiting for connection...\n");
+
+    }
+
 }
